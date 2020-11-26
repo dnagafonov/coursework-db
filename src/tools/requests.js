dee0f1703d9a8322a85e3df21efa576692ca5308
dnPC <denisagafonovwork.gmail.com>
2020-11-26 15:56:20 +0300

    update categories names

--- src/tools/requests.js
+++ src/tools/requests.js
@@ -1,10 +1,10 @@
 import db from "./firebase";
 import { errorToast } from "./toasts";
 import { normalizePath, sleep } from "./tools";
-import components from "./categories/components.json";
-import processors from "./categories/processors.json";
-import motherboards from "./categories/motherboards.json";
-import ram from "./categories/ram.json";
+import production from "./categories/production.json";
+import flowers from "./categories/flowers.json";
+import vegetables from "./categories/vegetables.json";
+import fruits from "./categories/fruits.json";
 
 const fetchCollection = async (collection) => {
   const snapshot = await db.collection(collection).get();
@@ -17,63 +17,51 @@ export const fetchDoc = async (path) => {
   return snapshot.data();
 };
 
-const getServerRAM3 = () => {
-  return fetchCollection("components/ram/server-ddr3");
+const getOther = () => {
+  return fetchCollection("production/vegetables/basic");
 };
 
-const getRAM3 = () => {
-  return fetchCollection("components/ram/ddr3");
+const getFruitsBasic = () => {
+  return fetchCollection("production/vegetables/basic");
 };
 
-const getRAM4 = () => {
-  return fetchCollection("components/ram/ddr4");
+const getFruitsExotic = () => {
+  return fetchCollection("production/vegetables/basic");
 };
 
-const getProcessorsLga2011 = () => {
-  return fetchCollection("components/processors/lga2011");
+const getFlowersBasic = () => {
+  return fetchCollection("production/flowers/basic");
 };
 
-const getProcessorsV3 = () => {
-  return fetchCollection("components/processors/v3");
+const getFlowersExostic = () => {
+  return fetchCollection("production/flowers/exotic");
 };
 
-const getMothersLga2011 = () => {
-  return fetchCollection("components/motherboards/lga2011");
+const getVegetablesBasic = () => {
+  return fetchCollection("production/vegetables/basic");
 };
 
-const getMothersV3 = () => {
-  return fetchCollection("components/motherboards/v3");
+const getVegetablesExotic = () => {
+  return fetchCollection("production/vegetables/exotic");
 };
 
-const getVideocards = () => {
-  return fetchCollection("/components/videocards/all");
-};
-
-const getSets = () => {
-  return fetchCollection("sets");
-};
-
-export const getComponents = (path) => {
+export const getproduction = (path) => {
   const npath = normalizePath(path);
   switch (npath) {
-    case "/components/processors/lga2011":
-      return getProcessorsLga2011();
-    case "/components/processors/v3":
-      return getProcessorsV3();
-    case "/components/motherboards/lga2011":
-      return getMothersLga2011();
-    case "/components/motherboards/v3":
-      return getMothersV3();
-    case "/components/ram/ddr3":
-      return getRAM3();
-    case "/components/ram/ddr4":
-      return getRAM4();
-    case "/components/ram/server-ddr3":
-      return getServerRAM3();
-    case "/components/videocards/all":
+    case "/production/flowers/basic":
+      return getFlowersBasic();
+    case "/production/flowers/exotic":
+      return getFlowersExostic();
+    case "/production/vegetables/basic":
+      return getVegetablesBasic();
+    case "/production/vegetables/exotic":
+      return getVegetablesExotic();
+    case "/production/fruits/basic":
+      return getFruitsBasic();
+    case "/production/fruits/exotic":
+      return getFruitsExotic();
+    case "/production/other":
       return getVideocards();
-    case "/sets":
-      return getSets();
     default:
       errorToast("Если видите эту ошибку, сообщите в поддержку в ВК");
   }
@@ -82,14 +70,14 @@ export const getComponents = (path) => {
 export const requestCategories = async (url) => {
   const nurl = normalizePath(url);
   switch (nurl) {
-    case "/components":
-      return await sleep(components, 300);
-    case "/components/processors":
-      return await sleep(processors, 300);
-    case "/components/motherboards":
-      return await sleep(motherboards, 300);
-    case "/components/ram":
-      return await sleep(ram, 300);
+    case "/production":
+      return await sleep(production, 300);
+    case "/production/flowers":
+      return await sleep(flowers, 300);
+    case "/production/vegetables":
+      return await sleep(vegetables, 300);
+    case "/production/fruits":
+      return await sleep(fruits, 300);
     default:
       errorToast("Если видите эту ошибку, сообщите в поддержку в ВК");
   }
